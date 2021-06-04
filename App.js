c566f8dacd1c006fc6cba6a8ab3118e11be2e63c
NIBIR <ntoasin@gmail.com>
2021-06-04 15:04:46 +0600

    first commit

--- App.js
+++ App.js
@@ -1,11 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>My name is Nibirsssss</Text>
+      <Image
+        style={styles.tinyLogo}
+        source={{
+          uri: 'https://c.ndtvimg.com/2019-10/kbn7tvqg_shakib-al-hasan_625x300_21_October_19.jpg?output-quality=60&output-format=webp&downsize=555:*',
+        }}
+        // style={{height: 100, width:100}}
+      />
       <StatusBar style="auto" />
     </View>
   );
@@ -18,4 +25,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  tinyLogo: {
+    height: 400, width:400
+  }
 });
